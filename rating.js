c4ec5294443a6ab4866c536e6156ab008399fcf9
Shreya Gupta <sfgupta@ucsd.edu>
2023-12-18 08:38:08 -0800

    web components

--- rating.js
+++ rating.js
@@ -1,56 +1,143 @@
-let submit = document.getElementById("rating-submit");
-let ratingForm = document.querySelector('rating-widget form');
-let ratingButtons = document.querySelectorAll('.star');
-let ratingOutputs = document.querySelectorAll('#delete-later output');
-let radioButtons2 = document.querySelectorAll('input[name="rating"]');
+class FeedbackWidget extends HTMLElement {
+    constructor() {
+        super();
+
+        
+
+        // Create a shadow root to encapsulate styles and elements.
+        this.attachShadow({ mode: 'open' });
+
+        // Create elements inside the shadow root.
+        this.h2 = document.createElement('h2');
+        this.h2.textContent = 'Feedback';
+        this.shadowRoot.appendChild(this.h2);
+
+        this.form = document.createElement('form');
+        this.form.action = 'https://httpbin.org/post';
+        this.form.method = 'POST';
+        this.shadowRoot.appendChild(this.form);
+
+        // Add hidden inputs
+        this.createHiddenInput('question', 'How satisfied are you?');
+        this.createHiddenInput('sentBy', 'Web Component');
+
+        // Create rating div with stars
+        this.ratingDiv = document.createElement('div');
+        this.ratingDiv.className = 'rating';
+        this.form.appendChild(this.ratingDiv);
+
+        for (let i = 5; i >= 1; i--) {
+            const input = document.createElement('input');
+            input.type = 'radio';
+            input.id = `star${i}`;
+            input.name = 'rating';
+            input.value = i;
+            this.ratingDiv.appendChild(input);
+
+            const label = document.createElement('label');
+            label.htmlFor = `star${i}`;
+            label.className = 'star';
+            this.ratingDiv.appendChild(label);
+        }
 
 
+        // Create submit button
+        
 
-submit.hidden = true;
-let prev_ind = 0;
+        const linkElem = document.createElement('link');
+        linkElem.setAttribute('rel', 'stylesheet');
+        linkElem.setAttribute('href', 'styles.css');
+        this.shadowRoot.appendChild(linkElem);
 
-ratingButtons.forEach((button, index) => {
-    button.addEventListener('mouseover', function() {
-      ratingOutputs[prev_ind].classList.remove('hovered');
-      let ind = 5 - index - 1
-      ratingOutputs[ind].classList.add('hovered');
-      prev_ind = ind;
-      
-    });
-  });
+        // Attach event listeners
+        this.ratingButtons = this.shadowRoot.querySelectorAll('.star');
+        this.ratingOutputs = this.shadowRoot.querySelectorAll('output');
+        this.radioButtons2 = this.shadowRoot.querySelectorAll('input[name="rating"]');
 
+    
+        this.prev_ind = 0;
 
+        this.ratingButtons.forEach((button, index) => {
+            button.addEventListener('mouseover', () => this.handleRatingHover(index));
+            button.addEventListener('click', () => this.handleRatingClick(index));
+        });
+    }
 
-  ratingButtons.forEach((button, index) => {
-    button.addEventListener('click', function() {
+    connectedCallback(){
+        // Access the rating-widget element inside the connectedCallback
+        var ratingWidget = document.querySelector('feedback-widget');
+
+        // Check if the rating-widget element is present
+        if (ratingWidget) {
+            // Iterate through child elements and hide them
+            for (var i = 0; i < ratingWidget.children.length; i++) {
+                ratingWidget.children[i].style.display = 'none';
+            }
+        }
+    }
+
+    createHiddenInput(name, value) {
+        const input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = name;
+        input.value = value;
+        this.form.appendChild(input);
+    }
+
+    handleRatingHover(index) {
+        // Hide all outputs\
+
+        const existingOutputs = this.form.querySelectorAll('output');
+        existingOutputs.forEach(output => output.remove());
+
+        index = 5 - index
+        const output = document.createElement('output');
+        output.class = "star"
+        output.htmlFor = `star${index}`;
+        if (index === 1){
+            output.textContent = `Thanks for your feedback of ${index} star. We'll try to do better`;
+        }
+        else if (index < 4) {
+            output.textContent = `Thanks for your feedback of ${index} stars. We'll try to do better`;
+        } else {
+            output.textContent = `Thanks for your feedback of ${index} star.`;
+        }
+    
+        this.form.appendChild(output);
+        
+    }
+    
+    handleRatingClick(index) {
         const xhr = new XMLHttpRequest();
         const url = "https://httpbin.org/post";
-
+    
         // Prepare form data
-        const formData = new FormData(ratingForm);
-        formData.set('sentBy', 'JS');  // Use set() to ensure the value is replaced or set explicitly
-        formData.set('rating', index); 
-
+        const formData = new FormData(this.form);
+        formData.set('sentBy', 'Web Component');
+        formData.set('rating', index + 1);
+    
         xhr.open("POST", url, true);
-
+    
         // Set headers
-        xhr.setRequestHeader('X-Sent-By', 'JS');
-
-        xhr.onreadystatechange = function() {
+        xhr.setRequestHeader('X-Sent-By', 'Web Component');
+    
+        xhr.onreadystatechange = () => {
             if (xhr.readyState === 4 && xhr.status === 200) {
                 console.log(JSON.parse(xhr.responseText));
-
-                // Loop through each radio button and set checked to false
-                radioButtons2.forEach(function(radioButton) {
-                    radioButton.checked = false;
-                });
-                ratingForm.submit();
+    
+                // Reset the form
+                this.form.reset();
+    
+                // Log a message or perform other actions after successful submission
+                console.log(`Feedback submitted successfully!`);
             }
         };
-
+    
         // Send the request
         xhr.send(formData);
-       
-       
-    });
-});
+        this.form.submit()
+    }
+}
+
+// Define the custom element
+customElements.define('feedback-widget', FeedbackWidget);
--- weather-service.js
+++ weather-service.js
@@ -1,90 +1,109 @@
+class WeatherWidget extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
 
-const weatherWidget = document.getElementById("weather");
-const weatherWidgetBig = document.getElementById("weatherWidget");
+        // Create elements inside the shadow root.
+        this.h2 = document.createElement('h2');
+        this.h2.textContent = 'Current Weather';
+        this.shadowRoot.appendChild(this.h2);
 
-let errorMsg = document.getElementById("weatherErrorMsg");
-if (errorMsg) {
-    errorMsg.style.display = "none";
+        this.weatherWidgetDiv = document.createElement('div');
+        this.weatherWidgetDiv.id = 'weatherWidget';
+        this.shadowRoot.appendChild(this.weatherWidgetDiv);
+
+        this.weatherDiv = document.createElement('div');
+        this.weatherDiv.id = 'weather';
+        this.weatherWidgetDiv.appendChild(this.weatherDiv);
+
+        this.weatherErrorMsg = document.createElement('p');
+        this.weatherErrorMsg.id = 'weatherErrorMsg';
+        this.weatherErrorMsg.textContent = 'Current Weather Conditions Unavailable';
+        this.weatherDiv.appendChild(this.weatherErrorMsg);
+
+        const linkElem = document.createElement('link');
+        linkElem.setAttribute('rel', 'stylesheet');
+        linkElem.setAttribute('href', 'styles.css');
+
+        this.shadowRoot.appendChild(linkElem);
+    }
+
+    // Function to update the weather widget with the provided weather data
+    updateWeatherWidget(weatherData) {
+        console.log("second-check");
+        // Your logic to update the weather widget with the provided data
+        // For simplicity, I'm calling fetchWeatherData here, but you might want to modify it based on your needs.
+        this.fetchWeatherData();
+    }
+
+    // Async function to fetch weather data
+    async fetchWeatherData() {
+        console.log("first-check");
+        const apiUrl = 'https://api.weather.gov/gridpoints/SGX/54,20/forecast/hourly?units=us';
+
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+
+            // Extract relevant information from the API response
+            const weatherData = data.properties.periods[0];
+
+            // Create HTML elements to display temperature, conditions, and icon
+            const temperatureElement = document.createElement('p');
+            const temperatureLabel = document.createElement('b');
+            temperatureLabel.textContent = 'Temperature:';
+            temperatureElement.appendChild(temperatureLabel);
+            temperatureElement.appendChild(document.createTextNode(` ${weatherData.temperature} °F`));
+
+            const conditionsElement = document.createElement('p');
+            const conditionsLabel = document.createElement('b');
+            conditionsLabel.textContent = 'Conditions:';
+            conditionsElement.appendChild(conditionsLabel);
+            conditionsElement.appendChild(document.createTextNode(` ${weatherData.shortForecast}`));
+
+            const rainElement = document.createElement('p');
+            const rainLabel = document.createElement('b');
+            rainLabel.textContent = 'Chance of Rain:';
+            rainElement.appendChild(rainLabel);
+            rainElement.appendChild(document.createTextNode(` ${weatherData.probabilityOfPrecipitation.value} %`));
+
+            const imgElement = document.createElement('img');
+            if (weatherData.shortForecast === "Sunny") {
+                imgElement.src = 'media/sun.svg';
+            } else if (weatherData.shortForecast === "Clear") {
+                imgElement.src = 'media/moon.svg';
+            } else if (weatherData.shortForecast === "Cloudy" || weatherData.shortForecast === "Mostly Cloudy") {
+                imgElement.src = 'media/cloud.svg';
+            } else if (weatherData.shortForecast.includes("Sunny")) {
+                imgElement.src = 'media/cloud-sun.svg';
+            } else if (weatherData.shortForecast.includes("Cloudy")) {
+                imgElement.src = 'media/cloud-moon.svg';
+            } else {
+                imgElement.src = 'media/cloud.svg';
+            }
+
+            imgElement.alt = 'weather icon';
+            imgElement.height = 80;
+            imgElement.width = 80;
+
+            // Clear existing content and append the new elements
+            this.weatherDiv.innerHTML = '';
+            this.weatherWidgetDiv.appendChild(imgElement);
+            this.weatherDiv.appendChild(temperatureElement);
+            this.weatherDiv.appendChild(conditionsElement);
+            this.weatherDiv.appendChild(rainElement);
+
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
+    }
+
+    // Fetch weather data when the component is attached to the DOM
+    connectedCallback() {
+        console.log("basic");
+        this.fetchWeatherData();
+    }
 }
 
-
-async function fetchWeatherData() {
-    console.log("first-check");
-    const apiUrl = 'https://api.weather.gov/gridpoints/SGX/54,20/forecast/hourly?units=us';
-
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-
-        // Extract relevant information from the API response
-        const weatherData = data.properties.periods[0];
-
-        // Create HTML elements to display temperature, conditions, and icon
-        const temperatureElement = document.createElement('p');
-        const temperatureLabel = document.createElement('b');
-        temperatureLabel.textContent = 'Temperature:';
-        temperatureElement.appendChild(temperatureLabel);
-        temperatureElement.appendChild(document.createTextNode(` ${weatherData.temperature} °F`));
-
-        const conditionsElement = document.createElement('p');
-        const conditionsLabel = document.createElement('b');
-        conditionsLabel.textContent = 'Conditions:';
-        conditionsElement.appendChild(conditionsLabel);
-        conditionsElement.appendChild(document.createTextNode(` ${weatherData.shortForecast}`));
-
-        const rainElement = document.createElement('p');
-        const rainLabel = document.createElement('b');
-        rainLabel.textContent = 'Chance of Rain:';
-        rainElement.appendChild(rainLabel);
-        rainElement.appendChild(document.createTextNode(` ${weatherData.probabilityOfPrecipitation.value} %`));
-        
-
-        const imgElement = document.createElement('img');
-        if (weatherData.shortForecast === "Sunny") {
-            imgElement.src = 'media/sun.svg';
-        }
-        else if (weatherData.shortForecast === "Clear") {
-            imgElement.src = 'media/moon.svg';
-        }
-        else if (weatherData.shortForecast === "Cloudy" || weatherData.shortForecast === "Mostly Cloudy") {
-            imgElement.src = 'media/cloud.svg';
-        }
-        else if (weatherData.shortForecast.includes("Sunny")) {
-            imgElement.src = 'media/cloud-sun.svg';
-        } 
-        else  if (weatherData.shortForecast.includes("Cloudy")) {
-            imgElement.src = 'media/cloud-moon.svg';
-        } else {
-            imgElement.src = 'media/cloud.svg';
-        }
-
-        imgElement.alt = 'weather icon';
-        imgElement.height = 80;
-        imgElement.width = 80;
-
-        // Clear existing content and append the new elements
-        weatherWidget.innerHTML = '';
-        weatherWidgetBig.appendChild(imgElement);
-        weatherWidget.appendChild(temperatureElement);
-        weatherWidget.appendChild(conditionsElement);
-        weatherWidget.appendChild(rainElement);
-        
-
-    } catch (error) {
-      console.error('Error fetching weather data:', error);
-
-      
-    }
-  }
-
-  // Function to update the weather widget with the provided weather data
-  function updateWeatherWidget(weatherData) {
-    console.log("second-check");
-    
-  }
-
-  // Fetch weather data when the page loads
-  window.addEventListener("load", (event) => {
-    console.log("basic");
-    fetchWeatherData()
-  });
+// Define the custom element
+customElements.define('weather-widget', WeatherWidget);
